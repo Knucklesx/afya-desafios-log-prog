e3737262e6ef6792a40e9757f76eb3e1263bbdca
Knucklesx <gcfacchi@gmail.com>
2024-06-05 09:19:01 -0300

    desafio-1

--- desafio-1.js
+++ desafio-1.js
@@ -3,8 +3,10 @@
 const numArray = [1, 2, 3, 4, 5];
 
 //Use a seguinte função para sua resposta
-function reverseArray() {}
+function reverseArray(x) {
+	return x.reverse();
+}
 
-reverseArray();
+reverseArray(numArray);
 
 module.exports = { reverseArray };
